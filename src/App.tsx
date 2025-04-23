f8973526c4ccda1835716ac598a0fdaeaa8ae2dc
Shorno <fb.shorno@gmail.com>
2025-04-23 07:23:01 +0600

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Todo from "./features/todo/Todo.tsx";
 
 function App() {
     return (
-        <div className={"flex items-center flex-col mt-32"}>
+        <div className={"flex items-center flex-col mt-10 sm:mt-32"}>
             <Todo/>
         </div>
     )
